9f6561f8cdb4a11a493eb99108bce4d54b568783
Bayu Luky <lukybayu@gmail.com>
2022-09-06 11:58:16 +0700

    update tdd and remove yarn

---
+++ components/__tests__/example.test.js
@@ -0,0 +1,9 @@
+describe('example test', () => {
+    it('passing test', () => {
+    expect(true).toBeTruthy();
+    })
+
+    it('failing test', () => {
+    expect(false).toBeFalsy();
+    })
+})
